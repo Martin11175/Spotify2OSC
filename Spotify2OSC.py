f650fcc62bc3e90780aa0f0306162bdc6d03b761
Martin11175 <martinhiggs85@googlemail.com>
2016-06-25 21:23:56 +0100

    Updated OAuth, confirmed applescript working.

--- Spotify2OSC.py
+++ Spotify2OSC.py
@@ -2,6 +2,8 @@
 import argparse
 import time
 import requests
+import string
+from requests.auth import HTTPBasicAuth
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
 from subprocess import Popen, PIPE
@@ -9,35 +11,50 @@ from subprocess import Popen, PIPE
 # Endpoint specification
 parser = argparse.ArgumentParser()
 parser.add_argument("--ip", default="127.0.0.1", help="The ip of the OSC server")
-parser.add_argument("--port", type=int, default=1604, help="The port the OSC server is listening on")
+parser.add_argument("--port", type=int, default=8000, help="The port the OSC server is listening on")
 args = parser.parse_args()
 
+CLIENT_ID = 'e90e8c6232b74444901ce298d64d28c9'
+CLIENT_KEY = 'df92dda22b4c4645af2991175a6ad62a'
+
 endpoint = udp_client.UDPClient(args.ip, args.port)
+login_hdr = HTTPBasicAuth(CLIENT_ID, CLIENT_KEY)
+auth = ""
 
 # Loop 5ever
 while True:
     # Applescript to retrieve current playing track ID from Spotify
-    scpt = '''
-        tell application "Spotify"
-            set currentTrackID to id of current track as string
-            return currentTrackID
-        end tell'''
-    args = []
+    #scpt = '''
+    #    tell application "Spotify"
+    #        set currentTrackID to id of current track as string
+    #        return currentTrackID
+    #    end tell'''
+    #args = []
 
     # Run Applescript and process output
-    p = Popen(['osascript', '-'] + args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = p.communicate(scpt)
-    track_id = stdout.decode()
+    #p = Popen(['osascript', '-'] + args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    #stdout, stderr = p.communicate(scpt)
+    #track_id = stdout.decode()
+    #track_id = track_id[14:]
+    track_id = "2fmIPKZ6dpko0N4MQuzH4N"
 
     # Request bpm information from Spotify Web API
-    auth = {"Authorization": "Bearer BQDEXKDMBLGhSU1sULRnmeO1aM9fkRhncog4fjfCMD9-"
-                             "Gw4dkb7qRPYU-aFwmF7kVDDIXMLk4eJNzt47Ecgww8SkYYy0Q1R"
-                             "3PqQyBbkBLG8NZ3qRTsmS8WYYQc3ZMEU4Fk_dr7Fsg77M"}
-    response = requests.get("https://api.spotify.com/v1/audio-features/" + track_id, headers=auth)
+    hdr = {"Authorization": "Bearer " + auth}
+    response = requests.get("https://api.spotify.com/v1/audio-features/" + track_id, headers=hdr)
+    if response.status_code != 200:
+        # If token timed out, re-authorise
+        auth_response = requests.post("https://accounts.spotify.com/api/token",
+                                      auth=login_hdr,
+                                      data={"grant_type": "client_credentials"})
+        auth = auth_response.json()["access_token"]
+        hdr = {"Authorization": "Bearer " + auth}
+        response = requests.get("https://api.spotify.com/v1/audio-features/" + track_id, headers=hdr)
+
     bpm = response.json()["tempo"]
+    print(bpm)
 
     # Construct and send OSC Packet
-    msg = osc_message_builder.OscMessageBuilder(address="/filter")
+    msg = osc_message_builder.OscMessageBuilder(address="/tempo")
     msg.add_arg(bpm)
     msg = msg.build()
     endpoint.send(msg)
