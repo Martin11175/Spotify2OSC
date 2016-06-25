4ce59ed1ccd642e0b86c5a09fb7df0fc25c382d7
Martin11175 <martinhiggs85@googlemail.com>
2016-06-25 19:34:15 +0100

    First completed version - untested

--- Spotify2OSC.py
+++ Spotify2OSC.py
@@ -1,25 +1,46 @@
+#!/usr/bin/env python3
+import argparse
+import time
+import requests
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
 from subprocess import Popen, PIPE
 
-# TODO: Port specification / default
+# Endpoint specification
+parser = argparse.ArgumentParser()
+parser.add_argument("--ip", default="127.0.0.1", help="The ip of the OSC server")
+parser.add_argument("--port", type=int, default=1604, help="The port the OSC server is listening on")
+args = parser.parse_args()
 
-# TODO: Loop
+endpoint = udp_client.UDPClient(args.ip, args.port)
 
-# TODO: Applescript to retrieve current playing track ID from Spotify
-scpt = '''
-    on run {x, y}
-        return x + y
-    end run'''
-args = ['2', '2']
+# Loop 5ever
+while True:
+    # Applescript to retrieve current playing track ID from Spotify
+    scpt = '''
+        tell application "Spotify"
+            set currentTrackID to id of current track as string
+            return currentTrackID
+        end tell'''
+    args = []
 
-# TODO: Run Applescript and process output
-p = Popen(['osascript', '-'] + args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
-stdout, stderr = p.communicate(scpt)
-print (p.returncode, stdout, stderr)
+    # Run Applescript and process output
+    p = Popen(['osascript', '-'] + args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = p.communicate(scpt)
+    track_id = stdout.decode()
 
-# TODO: Request bpm information from Spotify Web API
+    # Request bpm information from Spotify Web API
+    auth = {"Authorization": "Bearer BQDEXKDMBLGhSU1sULRnmeO1aM9fkRhncog4fjfCMD9-"
+                             "Gw4dkb7qRPYU-aFwmF7kVDDIXMLk4eJNzt47Ecgww8SkYYy0Q1R"
+                             "3PqQyBbkBLG8NZ3qRTsmS8WYYQc3ZMEU4Fk_dr7Fsg77M"}
+    response = requests.get("https://api.spotify.com/v1/audio-features/" + track_id, headers=auth)
+    bpm = response.json()["tempo"]
 
-# TODO: Process Spotify Web API response
+    # Construct and send OSC Packet
+    msg = osc_message_builder.OscMessageBuilder(address="/filter")
+    msg.add_arg(bpm)
+    msg = msg.build()
+    endpoint.send(msg)
 
-# TODO: Create OSC Packet
-
-# TODO: Send OSC packet to specified port
+    # Wait and go again
+    time.sleep(1)
